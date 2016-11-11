b5cfe4baab27a2d835e16d81be591f94e194e962
Celso Wo <celso.wo@tegra.me>
2016-11-11 13:35:54 -0200

    Removed songs

--- app/api/song.js
+++ app/api/song.js
@@ -3,14 +3,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-//   Song.find(function (err, songs) {
-
-//         if (err) {
-//             res.send(err);
-//         }
-
-//         res.json(songs);
-//     });
     res.json([
         {
             name: "Astronomy Domine",
@@ -18,6 +10,24 @@ router.get('/', function (req, res) {
             url: "/mp3/101-pink_floyd-astronomy_domine.mp3",
             image: "https://upload.wikimedia.org/wikipedia/pt/e/e1/Echoes_best_pink_floyd.jpeg"
         },
+        {
+            name: "See emily play",
+            artist: "Pink Floyd",
+            url: "/mp3/102-pink_floyd-see_emily_play.mp3",
+            image: "https://upload.wikimedia.org/wikipedia/pt/e/e1/Echoes_best_pink_floyd.jpeg"
+        },
+        {
+            name: "The happiest days of our lives",
+            artist: "Pink Floyd",
+            url: "/mp3/103-pink_floyd-the_happiest_days_of_our_lives.mp3",
+            image: "https://upload.wikimedia.org/wikipedia/pt/e/e1/Echoes_best_pink_floyd.jpeg"
+        },
+        {
+            name: "Another brick in the wall (part 2)",
+            artist: "Pink Floyd",
+            url: "/mp3/104-pink_floyd-another_brick_in_the_wall_(part_2).mp3",
+            image: "https://upload.wikimedia.org/wikipedia/pt/e/e1/Echoes_best_pink_floyd.jpeg"
+        }
     ]);
 }); 
 
