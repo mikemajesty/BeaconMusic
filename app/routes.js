905921704cc0a944d20a574fc35b2ea4fe37ec10
Willian Duque <willian.duque@tegra.me>
2016-11-11 14:39:34 -0200

    remove todos

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,3 @@
-var Todo = require('./models/todo');
-
-function getTodos(res) {
-    Todo.find(function (err, todos) {
-        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-        if (err) {
-            res.send(err);
-        }
-        res.json(todos); // return all todos in JSON format
-    });
-}
-
 module.exports = function (app) {
 
     app.use('/api/song', require('./api/song'));
