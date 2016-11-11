c1030a7e7eba888bbe650a204d74711c73375c2f
Willian Duque <willian.duque@tegra.me>
2016-11-11 16:55:48 -0200

    adds play/pause

--- public/player/js/controllers/main.js
+++ public/player/js/controllers/main.js
@@ -1,9 +1,8 @@
 angular.module('todoController', [])
 
 	.controller('mainController', ['$scope','$http', '$timeout', function($scope, $http, $timeout) {
-		
 		var audio = null;
-		
+
 		$scope.playlist = [];
 		$scope.currentSong = -1;
 		$scope.loading = true;
@@ -11,8 +10,7 @@ angular.module('todoController', [])
 			progress: 0,
 			duration: 0,
 			currentTime: 0
-		}
-
+		};
 		$scope.update = function() {
 			$http.get('/api/playlist').then(function(res) {
 				$scope.playlist = res.data;
@@ -23,8 +21,6 @@ angular.module('todoController', [])
 
 						audio = new Audio($scope.playlist[$scope.currentSong].url);
 						audio.play();
-
-						
 					}
 				} else {
 					$scope.audio.currentTime = parseInt(audio.currentTime / 60) +  ":" + parseInt(audio.currentTime % 60);
@@ -33,13 +29,25 @@ angular.module('todoController', [])
 					$scope.audio.progress = (audio.currentTime / audio.duration) * 100;
 				}
 			});
-
 			$timeout($scope.update, 2000);
 		};
+		$scope.paused = false;
+		$scope.pauseOrPlay = function(){
+			if (!$scope.paused) {
+				$scope.paused = true;
+	      audio.play();
+				document.getElementById("aplay").className = "ion-pause";
+	    } else {
+	       audio.pause();
+				 $scope.paused = false;
+				 document.getElementById("aplay").className = "ion-play";
+	    }
+		};
+
+
 
 		$scope.init = function() {
 			$timeout($scope.update, 2000);
 		};
-
 		$scope.init();
 	}]);
